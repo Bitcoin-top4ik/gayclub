509d769ed68889fbcdf49f7cbc886ad36f04b414
Bitcoin-top4ik <glusttsov@mail.ru>
2021-03-24 22:59:51 +0700

    gay
    gat

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    alert("Это новый гей клуб")
+    
+    var joinButton = document.getElementById("joinButton")
+    joinButton.addEventListener ("click", function(){
+        alert("gay")
+    })
+    }
